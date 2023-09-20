7d8e246f7be35ea01f7cf35072f6264a276aac63
hamza-69 <hamzarachidi04@gmail.com>
2023-09-20 19:10:38 +0300

    Start work on student performance project

---
+++ Student Performence/main.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+
+NUM_STUDENTS = 1000
+subjects = ["math", "science", "history", "english", "geography"]
+
+
+def load_report_card(directory, student_number):
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(directory, f"{student_number}.json")
+    path = os.path.join(base_path, file_path)
+
+    try:
+        with open(path, "r") as file:
+            report_card = json.load(file)
+    except FileNotFoundError:
+        return {}
+
+    return report_card
+
+print(load_report_card("students", 1))
